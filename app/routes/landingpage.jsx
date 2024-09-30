3442f3075e3fbd2c489dcf40ccd98d3f89cf0b4b
capsha1 <kevin.hoffman.france@gmail.com>
2024-09-30 21:33:06 +0200

    adding landing page and lib framer

--- app/routes/landingpage.jsx
+++ app/routes/landingpage.jsx
@@ -1,9 +1,11 @@
+// React
+import { motion } from "framer-motion"
+
+
 export default function LandingPage() {
     return (
-      <div>
-        <h1>Welcome to the Landing Page</h1>
-        <p className="mx-4">add scroll nimation with images, texts and zooms</p>
-      </div>
+
+      <motion.div />     
   
       
     );
