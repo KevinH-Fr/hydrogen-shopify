f2901e4e0e5967635e6a5d98155ee295cf84c666
capsha1 <kevin.hoffman.france@gmail.com>
2024-09-30 21:54:57 +0200

    basic framer element with animation

---
+++ app/components/MotionBox2.jsx
@@ -0,0 +1,18 @@
+// src/MotionBox.js
+import { motion } from 'framer-motion';
+
+const MotionBox1 = ({ text }) => {
+  return (
+    <motion.div
+      className="bg-red-500 text-white p-40 rounded text-center my-10" // Added margin-bottom for spacing
+      initial={{ opacity: 0, y: -50 }} // Initial state
+      animate={{ opacity: 1, y: 0 }}    // Animate to this state
+      exit={{ opacity: 0, y: 50 }}      // Exit animation
+      transition={{ duration: 2 }}     // Transition settings
+    >
+      {text}  {/* Display text passed as a prop */}
+    </motion.div>
+  );
+};
+
+export default MotionBox1;
